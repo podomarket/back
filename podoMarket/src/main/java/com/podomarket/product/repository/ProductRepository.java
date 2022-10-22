3a28ea8b89b2efadc36923680c99667d2c2ce9bd
강희인 <dwoosung1@gmail.com>
2022-10-22 15:16:12 +0900

    "crud Product(Login x)"

--- podoMarket/src/main/java/com/podomarket/product/repository/ProductRepository.java
+++ podoMarket/src/main/java/com/podomarket/product/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.podomarket.product.repository;
 import com.podomarket.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByOrderByModifiedAtDesc();
 }
